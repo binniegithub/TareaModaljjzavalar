7d2596c1428048777f2ea6d498e01d92ed7755ba
binniegithub <98235974+binniegithub@users.noreply.github.com>
2022-05-21 11:46:36 -0600

    Update app.js

--- Operaciones CRUD con formulario modal/modal - Ejercicio 2/js/app.js
+++ Operaciones CRUD con formulario modal/modal - Ejercicio 2/js/app.js
@@ -48,16 +48,28 @@ function InsertarRegistro(){
         var e = a.insertCell(3);
         var f = a.insertCell(4);
         var g = a.insertCell(5);
+        var h = a.insertCell(6);
 
-        b.innerHTML = document.getElementById("IDTextBox").value;
-        c.innerHTML = document.getElementById("FirstNameTextBox").value;
-        d.innerHTML = document.getElementById("LastNameTextBox").value;
-        e.innerHTML = GetCheckedRadioValue("Sexo");
-        f.innerHTML = GetCheckedBoxesValue("Pasatiempos");
+        c.innerHTML = document.getElementById("IDTextBox").value;
+        d.innerHTML = document.getElementById("FirstNameTextBox").value;
+        e.innerHTML = document.getElementById("LastNameTextBox").value;
+
+        var genero = GetCheckedRadioValue("Sexo");
+        f.innerHTML = genero;
+
+        if(genero=="Masculino"){
+            b.className = "gendermale";
+
+        }
+        else{
+            b.className = "genderfemale";
+        }
+
+        g.innerHTML = GetCheckedBoxesValue("Pasatiempos");
         var buttons = "<button name=EditButton' onclick='ShowModalForEdit(this)'>Editar</button>";
         buttons = buttons.concat("&nbsp;").concat("<button name = 'DeleteButton' onclick='deleteRow(this)'>Eliminar</button>");
         
-        g.innerHTML = buttons;
+        h.innerHTML = buttons;
 
         var modal = document.getElementById("myModal");
         modal.style.display = "none";
@@ -73,13 +85,13 @@ function ShowModalForEdit(sender){
     modal.style.display = "block";
     selectedRowIndex = sender.parentNode.parentNode.rowIndex;
 
-    document.getElementById("IDTextBox").value = table.rows[selectedRowIndex].cells[0].innerHTML.toString();
-    document.getElementById("FirstNameTextBox").value = table.rows[selectedRowIndex].cells[1].innerHTML.toString();
-    document.getElementById("LastNameTextBox").value = table.rows[selectedRowIndex].cells[2].innerHTML.toString();
-    var genero = table.rows[selectedRowIndex].cells[3].innerHTML.toString();
+    document.getElementById("IDTextBox").value = table.rows[selectedRowIndex].cells[1].innerHTML.toString();
+    document.getElementById("FirstNameTextBox").value = table.rows[selectedRowIndex].cells[2].innerHTML.toString();
+    document.getElementById("LastNameTextBox").value = table.rows[selectedRowIndex].cells[3].innerHTML.toString();
+    var genero = table.rows[selectedRowIndex].cells[4].innerHTML.toString();
     SelectRadioByValue("Sexo",genero);
 
-    var PasatiemposString = table.rows[selectedRowIndex].cells[4].innerHTML.toString();
+    var PasatiemposString = table.rows[selectedRowIndex].cells[5].innerHTML.toString();
     var PasatiemposArray = PasatiemposString.split(",");
 
     for (var x=0;x<PasatiemposArray.length;x++){
@@ -134,10 +146,20 @@ function SelectCheckBoxByValue(ChkboxName,Value){
 }
 
 function ActualizarRegistro(){
-    table.rows[selectedRowIndex].cells[0].innerHTML = document.getElementById("IDTextBox").value;
-    table.rows[selectedRowIndex].cells[1].innerHTML = document.getElementById("FirstNameTextBox").value;
-    table.rows[selectedRowIndex].cells[2].innerHTML = document.getElementById("LastNameTextBox").value;
-    table.rows[selectedRowIndex].cells[3].innerHTML = GetCheckedRadioValue("Sexo");
-    table.rows[selectedRowIndex].cells[4].innerHTML = GetCheckedBoxesValue("Pasatiempos");
+    table.rows[selectedRowIndex].cells[1].innerHTML = document.getElementById("IDTextBox").value;
+    table.rows[selectedRowIndex].cells[2].innerHTML = document.getElementById("FirstNameTextBox").value;
+    table.rows[selectedRowIndex].cells[3].innerHTML = document.getElementById("LastNameTextBox").value;
+
+    var genero = GetCheckedRadioValue("Sexo");
+    table.rows[selectedRowIndex].cells[4].innerHTML=genero;
+    if(genero=="Masculino"){
+        table.rows[selectedRowIndex].cells[0].className = "gendermale";
+
+    }
+    else{
+        table.rows[selectedRowIndex].cells[0].className = "genderfemale";
+    }
+    table.rows[selectedRowIndex].cells[4].innerHTML = GetCheckedRadioValue("Sexo");
+    table.rows[selectedRowIndex].cells[5].innerHTML = GetCheckedBoxesValue("Pasatiempos");
     modal.style.display = "none";
 }
